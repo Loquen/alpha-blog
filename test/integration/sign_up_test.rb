fca580848d0421b7eeade23cc6e0816c1d909a98
Rails Udemy Course <railsudemycourse@gmail.com>
2017-12-27 05:10:48 +0000

    Added new test for invalid username failure

--- test/integration/sign_up_test.rb
+++ test/integration/sign_up_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class SignUpTest < ActionDispatch::IntegrationTest
 
   def setup
-    # @user = User.create(username: "john", email: "john@mail.com", password: "password")
     @username = "john"
     @email = "john@mail.com"
     @password = "password"
-    # @user1 = User.create(username: "jo", email: "john@mail.com", password: "password")
-    # @user2 = User.create(username: "john", email: "john@mail.com", password: "password", admin: true)
+    @invalidusername = "jo"
+    @email1 = "john"
+    @password1 = ""
   end
 
   test "get sign up form and sign up" do
@@ -17,8 +17,22 @@ class SignUpTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', 1 do # verify that there is the User count increases upon User creation
       @user = User.create(username: @username, email: @email, password: @password)
     end
-    # assert_template "users/#{@user.id}"
     assert_match @user.username, User.last.username # check to make sure the new user has been created and exists
+  end
+  
+  test "invalid username results in failure" do
+    get signup_path
+    assert_template 'users/new'
+    assert_no_difference 'User.count' do
+      @user = User.create(username: @invalidusername, email: @email, password: @password)
+    end
+    assert_template 'users/new'
+    assert_no_difference 'User.count' do
+      post users_path, user: {username: "j", email: "lo@gm.cm", password: "password" }
+    end
+    assert_match "Username is too short", response.body
+    assert_select 'h2.panel-title' # not able to find this because the signup is not actually happening through the page
+    assert_select 'div.panel-body'
   end
 
 end
